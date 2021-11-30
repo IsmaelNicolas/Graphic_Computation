2d553fbc78b8d4e28fa345603f43a8f86b331e85
Ismael Nicolas <ismael.cedillo@outlook.es>
2021-11-30 17:47:39 -0500

    Calculate external and internal points

--- Tarea 1 Primer Parcial/WinAppStarPoligon/WinAppStarPoligon/CStarPoligon.cs
+++ Tarea 1 Primer Parcial/WinAppStarPoligon/WinAppStarPoligon/CStarPoligon.cs
@@ -17,7 +17,7 @@ namespace WinAppStarPoligon
         private LinkedList<PointF> mMiddlePoligon = new LinkedList<PointF>();
         private LinkedList<PointF> mInnerPoligon = new LinkedList<PointF>();
         private Graphics mGraph;
-        private const float SF = 50; // SCALE FACTOR
+        private const float SF = 100; // SCALE FACTOR
         private Pen mPencil;
         #endregion
 
@@ -41,52 +41,91 @@ namespace WinAppStarPoligon
 
         #region Methods
 
-        public void initialize_data()
+        private void initialize_data()
         {
-            float[] sin = { (float)Math.Sin(3 * Math.PI / 10) , (float)Math.Sin(Math.PI / 10) };
-            float[] cos = { (float)Math.Cos(3 * Math.PI / 10) , (float)Math.Cos(Math.PI / 10) };
-            OuterPoligon.AddLast(new PointF(0.0f, 0.0f ));
-            OuterPoligon.AddLast(new PointF(1.0f , 0.0f));
-            //OuterPoligon.AddLast( new PointF( (1+ (float)Math.Sin(3*Math.PI/10) )*SF , ((float)Math.Cos(3 * Math.PI / 10))*SF));
+            //OuterPoligon.AddLast(new PointF(0.0f, 0.0f ));
+            //OuterPoligon.AddLast(new PointF(1.0f , 0.0f));
 
-            int n = 0,k=0;
-            Func<int, int> s = (x) => Math.Abs((int)Math.Round(Math.Sin(x)));
+        }
 
-            for (int i = 2,j=0; i<4; i++,j++)
+
+        private void calculate_points(LinkedList<PointF> link,PointF origin,float module, double[] deg)
+        {
+
+            double[] angles = deg;
+            Func<int, int> r = (x) => (int)Math.Round((double)(x / 3));
+            int i, j, n, k;
+
+           // if(angles[0] == 3 * Math.PI / 10)
+            //{
+                i = 0; j = 2; n = 1; k = 0;
+                link.AddLast(new PointF(0.0f, 0.0f));
+                link.AddLast(new PointF(1, 0.0f));
+            //}
+            /*else
             {
-                
-                OuterPoligon.AddLast(new PointF(
-                    OuterPoligon.ElementAt(i - 1).X + ((float)Math.Pow(-1,k))*sin[n],
-                    OuterPoligon.ElementAt(i - 1).Y + cos[n] 
-                ));
+                i = 0; j = 1; n = 1; k = 0;
+                link.AddLast(new PointF(0.0f, 0.0f));
+            }*/
+            
 
-                n = s(j+1);
-                k = s(j);
+            while (j < 10)
+            {
+                if (j != 6)
+                {
+                    i = i > 3 ? 0 : i += 1;
+                    n = n > 3 ? 0 : n += 1;
+                    link.AddLast(new PointF(
+                            link.ElementAt(j - 1).X + ((int)Math.Pow(-1, k)) * ((int)Math.Pow(-1, r(i))) * (float)Math.Sin(angles[r(n)]),
+                            link.ElementAt(j - 1).Y + ((int)Math.Pow(-1, k)) * (float)Math.Cos(angles[r(n)])
+                    ));
+
+                    j++;
+                }
+                else
+                {
+                    link.AddLast(new PointF(0, link.ElementAt(j - 1).Y));
+                    j++; i = 0; n = 1; k = 1;
+                }
+
+            }
+
+            foreach (PointF p in link)
+            {
+                link.Find(p).Value = new PointF((p.X*module + origin.X) * SF, (p.Y*module + origin.Y) * SF);
             }
 
         }
 
-        public void plot_figure(PictureBox canvas,Label control)
+        public void plot_figure(PictureBox canvas, Label control)
         {
             Graph = canvas.CreateGraphics();
             Brush aBrush = (Brush)Brushes.Red;
             Brush aBrush1 = (Brush)Brushes.Blue;
-            Pencil = new Pen(Color.Blue);
-            foreach(PointF p in OuterPoligon)
-            {
-                control.Text += p.ToString() +"\n";
-            }
-            //control.Text = OuterPoligon.ToString();
-            //Graph.FillRectangle(aBrush, Points.ElementAt(0).X, Points.ElementAt(0).Y,SF, SF);
-            //foreach(PointF p in OuterPoligon)
-            //{
-            //    Graph.FillRectangle(aBrush1, p.X*SF, p.Y *SF, 3, 3);
-            //}
-            //Graph.DrawPolygon(Pencil, OuterPoligon.ToArray());
-            //Graph.DrawLine(Pencil, OuterPoligon.ElementAt(0), OuterPoligon.ElementAt(1));
-            //Graph.FillRectangle(aBrush, OuterPoligon.ElementAt(1).X *SF, OuterPoligon.ElementAt(1).Y *SF, 3, 3);
+            Pencil = new Pen(Color.Black, 3);
+            PointF origin = new PointF(1.25f, 0);
+            
+            double[] deg = { 3 * Math.PI / 10, Math.PI / 10 };
+            double[] deg1 = { 2 * Math.PI / 5, Math.PI / 5 };
+
+            calculate_points(OuterPoligon, origin, 1, deg );
+            calculate_points(InnerPoligon, new PointF(origin.X + 0.325f,origin.Y + 1) ,0.35f,deg);
+            //calculate_points(MiddlePoligon, new PointF(origin.X + 0.5f,origin.Y + 0.5f) ,0.63f,deg);
+
+
+            Graph.DrawPolygon(Pencil, OuterPoligon.ToArray());
+            Graph.DrawPolygon(Pencil, InnerPoligon.ToArray());
+            //Graph.DrawPolygon(Pencil, MiddlePoligon.ToArray());
+
+
             Func<int, int> r = (x) => (int)Math.Round((double)(x/3));
             int i = 0,j=0,n=1;
+
+            foreach (var p in MiddlePoligon)
+            {
+                control.Text += (p.X/SF).ToString() + "," + (p.Y / SF).ToString() + "\n"; 
+            }
+
             while (j<9) 
             {
                 if (j != 4){
@@ -97,7 +136,7 @@ namespace WinAppStarPoligon
                 }
                 else
                 {
-                    j++;i = 0;n = 1;
+                    j++;i = 2;n = 1;
                     control.Text += "Cambio"+ "\n";
                 }
                 
